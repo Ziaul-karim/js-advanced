92eae2d2b7d374b70660df42a543bf20a32e5680
Ziaul karim <karimziaul62@gmail.com>
2020-06-13 06:09:25 +0600

    map done

---
+++ map.js
@@ -0,0 +1,24 @@
+const nums = [3, 7, 9, 4, 6, 1];
+// const num2 = [];
+
+// for(let i = 0; i < nums.length; i++){
+//     const element = nums[i];
+//     const res = element * element;
+//     num2.push(res);
+// }
+
+// function square(element){
+//     return element * element;
+// }
+
+// const square = element => element * element;
+// const square = x => x * x;
+
+
+
+// const res = nums.map(function(element, index){
+//     return element * element;
+// });
+
+const res = nums.map(x => x * x);
+console.log(res);
